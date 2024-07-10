3e2f83c0fb5a3f6defe6b8c9a25348f1409a035e
Mammad Chiragli <mammadchiragli@MacBook-Air--Seva.local>
2024-07-11 00:52:11 +0400

    prompt alert

---
+++ index.js
@@ -0,0 +1,33 @@
+const name = prompt ('Enter your name, please!')
+alert('Hello,Seva!')
+
+// const date = prompt ('Enter your date of birth (only year)')
+// alert(2024-date)
+
+// const number = prompt ('Enter length of square')
+// alert(number * number)
+
+// const number = prompt('Enter the radius of the circle')
+// alert(number * number * 3.14159)
+
+// const distance = prompt ('Enter the distance in kilometers between cities')
+// const time = prompt('How many hours you want to take to reach there')
+// alert(distance / time)
+
+// const currency = prompt('Enter the amounts in dollars')
+// alert (currency * 0.92)
+
+// const volume = prompt ('Enter the volume of flash drive in gugabytes')
+// alert(volume * 1024 / 820)
+
+// const money = Number(prompt('Enter the amount of your money in your wallet'));
+// const price = Number(prompt('Enter the price of one chocolate'));
+// const chocolatesCount = Math.floor(money / price);
+// const change = money % price;
+// alert(`You can buy ${chocolatesCount} chocolates. Your changes: ${change}`);
+
+// const number = prompt('Enter three-digit number')
+// alert(number%10)
+
+// const number = prompt('Enter integer number')
+
